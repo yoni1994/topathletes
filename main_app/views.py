18f3bd802383254fa42730233e67bd4d7679862d
yoni1994 <yoniklein1994@gmail.com>
2021-08-25 11:37:53 -0500

    add team class

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 # Create your views here.
+
+class Team:
+    def __init__(self, city, name, sport, primaryColor, secondaryColor):
+        self.city = city
+        self.name = name
+        self.sport = sport
+        self.primaryColor = primaryColor
+        self.secondaryColor = secondaryColor
+    
+teams = [
+    Team('Chicago', 'Cubs', 'baseball', 'blue', 'red'),
+    Team('Chicago', 'Bulls', 'basketball', 'red', 'black'),
+    Team('Chicago', 'Bears', 'football', 'navy', 'orange'),
+    Team('Chicago', 'Blackhawks', 'hockey', 'red', 'black'),
+]
+
+
+
 def home(request):
     return HttpResponse('<h1>Welcome to Top Athletes Home Page')
 
