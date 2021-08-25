da1ded93811b5382e7640f99c9b1cf5038c9650d
yoni1994 <yoniklein1994@gmail.com>
2021-08-25 14:25:56 -0500

    make team and player models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    def __init__(self, city, name, sport, primaryColor, secondaryColor):
+        city = models.CharField(max_length=25)
+        name = models.CharField(max_length=20)
+        sport = models.CharField(max_length=10)
+        primaryColor = models.CharField(max_length=20)
+        secondaryColor = models.CharField(max_length=20)
+    
+# teams = [
+#     Team('Chicago', 'Cubs', 'baseball', 'blue', 'red'),
+#     Team('Chicago', 'Bulls', 'basketball', 'red', 'black'),
+#     Team('Chicago', 'Bears', 'football', 'navy', 'orange'),
+#     Team('Chicago', 'Blackhawks', 'hockey', 'red', 'black'),
+# ]
+
+class Player(models.Model):
+    def __init__(self, name, sport, position, height, team):
+        name = models.CharField(max_length=50)
+        sport = models.CharField(max_length=10)
+        position = models.CharField(max_length=25)
+        height = models.CharField(max_length=10)
+        team = models.CharField(max_length=50)
+    
+# players = [
+#     Player('Ernie Banks', 'baseball', 'shortstop', '6\'1', 'Chicago Cubs'),
+#     Player('Derrick Rose', 'basketball', 'point guard', '6\'3', 'Chicago Bulls'),
+#     Player('Devin Hester', 'football', 'wide receiver', '5\'11', 'Chicago Bears'),
+#     Player('Jonathan Toews', 'hockey', 'center', '6\'2', 'Chicago Blackhawks'),
+# ]
+
--- main_app/views.py
+++ main_app/views.py
@@ -35,6 +35,7 @@ players = [
 ]
 
 
+
 def home(request):
     return render(request, 'home.html')
 
