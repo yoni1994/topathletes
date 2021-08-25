05a31ad4701d69b7d904964ae87f99cf82d3457f
yoni1994 <yoniklein1994@gmail.com>
2021-08-25 11:49:46 -0500

    add player and team routes

--- main_app/views.py
+++ main_app/views.py
@@ -51,3 +51,8 @@ def football(request):
 def hockey(request):
     return render(request, 'hockey.html')
 
+def teams_index(request):
+    return render(request, 'teams/index.html', { 'teams': teams })
+
+def players_index(request):
+    return render(request, 'players/index.html', { 'players': players })
