3a22c0a6d110eeae6ca188f3599386c94dc3263e
yoni1994 <yoniklein1994@gmail.com>
2021-08-25 10:28:02 -0500

    add baseball, basketball, football, hockey views

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('baseball/', views.baseball, name='baseball'),
+    path('basketball/', views.basketball, name='basketball'),
+    path('football/', views.football, name='football'),
+    path('hockey/', views.hockey, name='hockey'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Welcome to Top Athletes Home Page')
+
+def baseball(request):
+    return HttpResponse('<h1>Welcome to Top Athletes Baseball Page')
+
+def basketball(request):
+    return HttpResponse('<h1>Welcome to Top Athletes Basketball Page')
+
+def football(request):
+    return HttpResponse('<h1>Welcome to Top Athletes Football Page')
+
+def hockey(request):
+    return HttpResponse('<h1>Welcome to Top Athletes Hockey Page')
+
