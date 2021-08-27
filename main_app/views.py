e6b3bed529123eb369692314f6d800ab7c338ddb
yoni1994 <yoniklein1994@gmail.com>
2021-08-27 14:38:06 -0500

    import updateView and deleteView

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Team, Player
 
 
