ee800ae3e837e8e76f0e5647aae1169ad4e8ce9c
yoni1994 <yoniklein1994@gmail.com>
2021-08-29 21:11:50 -0500

    change dbs

--- topathletes/settings.py
+++ topathletes/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'topathletes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'topathletes',
     }
 }
 
